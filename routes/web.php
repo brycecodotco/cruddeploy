2aa4b332846e00c0a266faf3221ce80aed95f41e
brycecodotco <gxcreeper@gmail.com>
2026-04-22 13:45:55 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
 
+Route::get('/', function () {
+    return redirect('/books');
+});
+
+
 Route::get('/books',[BooksController::class,'index']);
 Route::post('/storebook',[BooksController::class,'store']);
 Route::get('/books/{id}/edit',[BooksController::class,'edit']);
